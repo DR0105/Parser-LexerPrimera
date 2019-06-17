5290bca73a45e3a1a57b3d6759ce2f1a9d8b11ae
DR0105 <36673480+DR0105@users.noreply.github.com>
2019-06-17 01:24:50 -0500

    Update lexer_prueba.py

--- lexer_prueba.py
+++ lexer_prueba.py
@@ -1,9 +1,15 @@
 import lexer_rules
 from ply.lex import lex
-text = "(14 + 6) * 2"
 lexer = lex(module=lexer_rules)
-lexer.input(text)
-token = lexer.token()
-while token is not None:
-    print token.type, token.value
-    token = lexer.token()
+f= open ('operaciones.txt','r')
+o=f.read()
+f.close()
+oper=o.splitlines()
+i=0
+while i<len(oper):
+   lexer.input(oper[i])
+   token = lexer.token()
+   while token is not None:
+       print token.type, token.value
+       token = lexer.token()
+   i=i+1
